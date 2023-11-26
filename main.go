6af1660f9ab9d0f6414c45f0f0ab49b093cc11b7
fourdim <59462000+fourdim@users.noreply.github.com>
2023-11-26 02:12:38 -0500

    init: commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+    conn, err := net.Dial("tcp", "10.10.10.10:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
+}
+
+func headersByRequestURI() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if strings.HasPrefix(c.Request.RequestURI, "/dl") {
+			c.Header("Content-Disposition", "attachment")
+		}
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.Use(headersByRequestURI())
+	router.StaticFS("/dl", http.Dir("./dl"))
+	router.StaticFile("/favicon.ico", "./static/favicon.ico")
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/list")
+	})
+	router.GET("/list/*action", func(c *gin.Context) {
+		action := c.Param("action")
+		actionDir, err := os.ReadDir("./dl/" + action)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+		var files []string
+		var dirs []string
+		for _, ele := range actionDir {
+			if ele.IsDir() {
+				dirs = append(dirs, ele.Name())
+			} else {
+				files = append(files, ele.Name())
+			}
+		}
+		var path string
+		if action == "/" {
+			path = ""
+		} else {
+			path = action
+		}
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title":  "临时下载站",
+			"action": path,
+			"files":  files,
+			"dirs":   dirs,
+		})
+	})
+
+	println(GetOutboundIP())
+
+	router.Run(":28100")
+}
